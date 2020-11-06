0c13a41e174e1f1615410c6359658da66a108e26
lemon-clown <me.clown.lemon@gmail.com>
2020-11-06 22:25:49 +0800

    :sparkles:  feat: implement new component @yozora/react-delete

---
+++ packages/delete/README.md
@@ -0,0 +1,41 @@
+[![npm version](https://img.shields.io/npm/v/@yozora/react-delete.svg)](https://www.npmjs.com/package/@yozora/react-delete)
+[![npm download](https://img.shields.io/npm/dm/@yozora/react-delete.svg)](https://www.npmjs.com/package/@yozora/react-delete)
+[![npm license](https://img.shields.io/npm/l/@yozora/react-delete.svg)](https://www.npmjs.com/package/@yozora/react-delete)
+
+
+This library is designed to render [mdast delete][] type data
+
+
+# Install
+
+  ```shell
+  yarn add @yozora/react-delete
+  ```
+
+# Usage
+  * Use in React project
+
+    ```typescript
+    // index.tsx
+    import React from 'react'
+    import ReactDOM from 'react-dom'
+    import Delete from '@yozora/react-delete'
+
+    ReactDOM.render(
+      <Delete>
+        <span>Hello, world!</span>
+      </Delete>
+      , document.getElementById('root')
+    )
+    ```
+
+  * Props
+
+     Name       | Type                                | Required  | Default | Description
+    :----------:|:-----------------------------------:|:---------:|:-------:|:-------------
+     `children` | `React.ReactNode`                   | `true`    | `-`     | Delete content
+     `ref`      | `React.RefObject<HTMLSpanElement>`  | `false`   | `-`     | Forwarded ref callback
+
+    Delete Props inherited all attributes of `HTMLSpanElement` (`React.HTMLAttributes<HTMLSpanElement>`)
+
+[mdast delete]: https://github.com/syntax-tree/mdast#delete
---
+++ packages/delete/__test__/delete.spec.tsx
@@ -0,0 +1,65 @@
+import React from 'react'
+import { mount, shallow } from 'enzyme'
+import Delete from '../src'
+
+
+describe('test suite: Test component', () => {
+  const errorLogger = jest
+    .spyOn(global.console, 'error')
+    .mockImplementation((...args) => {
+      throw new Error(args.join(' '))
+    })
+
+  afterAll(() => {
+    errorLogger.mockRestore()
+  })
+
+  it('expect render a simple content', () => {
+    const text = 'Hello, world!'
+    const wrapper = shallow(
+      <Delete>
+        <span>
+          <Delete>{ text }</Delete>
+        </span>
+      </Delete>
+    )
+    expect(wrapper.text()).toEqual(text)
+  })
+
+  it('expect render with custom className', () => {
+    const text = 'Hello, world!'
+    const className = 'custom-text'
+    const wrapper = shallow(
+      <Delete className={ className }>
+        <span>{ text }</span>
+      </Delete>
+    )
+    expect(wrapper.hasClass(className)).toEqual(true)
+    expect(wrapper.text()).toEqual(text)
+  })
+
+  it('children is required', () => {
+    for (const value of [undefined, null] as any[]) {
+      expect(() => {
+        mount(
+          <Delete>
+            { value }
+          </Delete>
+        )
+      }).toThrow(/Failed prop type: The prop `children` is marked as required/)
+    }
+  })
+
+  it('forward ref', () => {
+    const ref = React.createRef<HTMLSpanElement>()
+    const wrapper = mount(
+      <Delete ref={ ref } data-value="waw">
+        1
+      </Delete>
+    )
+
+    const o = wrapper.getDOMNode()
+    expect(o).toEqual(ref.current)
+    expect(o.getAttribute('data-value')).toEqual('waw')
+  })
+})
