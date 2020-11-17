33e91ea5c802621c6b2d935a825d775d0c520e8a
guanghechen <me.clown.lemon@gmail.com>
2020-11-17 22:48:33 +0800

    :sparkles:  feat: implement new component @yozora/react-markdown

---
+++ packages/markdown/src/block/code/live.tsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import { CodeLive } from '@yozora/react-code-live'
+import { CodeRenderer } from './embed'
+
+
+/**
+ * Props for code live
+ */
+export interface CodeLiveRendererProps {
+  /**
+   * Code language
+   */
+  lang: string
+  /**
+   * Source code contents
+   */
+  value: string
+  /**
+   *
+   */
+  args?: Record<string, unknown>
+}
+
+
+/**
+ *
+ * @param props
+ */
+export function CodeLiveRenderer(props: CodeLiveRendererProps): React.ReactElement | null {
+  const { lang, value } = props
+
+  return (
+    <CodeLive
+      lang={ lang }
+      value={ value }
+      CodeRenderer={ CodeRenderer }
+    />
+  )
+}
+
+
+export default CodeLiveRenderer
---
+++ packages/markdown/src/index.tsx
@@ -0,0 +1,80 @@
+import React from 'react'
+import MathJax from 'react-mathjax'
+import PropTypes from 'prop-types'
+import styled, { DefaultTheme, ThemeProvider } from 'styled-components'
+import MdastRenderer, { MdastRendererProps } from './ast/render'
+import type { MdastPropsRoot } from './ast/types'
+import { defaultTheme } from './theme'
+export * from './ast/render'
+export * from './ast/types'
+export * from './theme'
+
+
+/**
+ * Props for Markdown
+ */
+interface MarkdownProps extends React.HTMLAttributes<HTMLDivElement> {
+  /**
+   * Markdown content
+   */
+  ast: MdastPropsRoot
+  /**
+   * Styled-components theme for @yozora/react-* components
+   */
+  theme?: DefaultTheme
+  /**
+   * Markdown renderer
+   */
+  render?: (props: MdastRendererProps) => React.ReactElement
+}
+
+
+const Container = styled.div`
+  display: block;
+  font-size: 1rem;
+  line-height: 1.4;
+  :last-child {
+    margin-bottom: 0;
+  }
+`
+
+
+/**
+ *
+ * @param props
+ */
+export const Markdown = React.forwardRef<HTMLDivElement, MarkdownProps>(
+  (props, forwardRef): React.ReactElement => {
+    const {
+      ast,
+      theme = defaultTheme,
+      render: Markdown = MdastRenderer,
+      ...htmlProps
+    } = props
+    return (
+      <Container { ...htmlProps } ref={ forwardRef }>
+        <ThemeProvider theme={ theme }>
+          <MathJax.Provider>
+            <Markdown ast={ ast } />
+          </MathJax.Provider>
+        </ThemeProvider>
+      </Container>
+    )
+  }
+)
+
+
+Markdown.displayName = 'Markdown'
+
+
+Markdown.propTypes = {
+  ast: PropTypes.any.isRequired,
+  theme: PropTypes.any,
+  render: PropTypes.oneOfType<any>([
+    PropTypes.elementType,
+    PropTypes.func,
+  ]),
+}
+
+
+export default Markdown
