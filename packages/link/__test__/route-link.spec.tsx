91b3768ef1480a4cc087f12f5f46f48fc0a81a55
guanghechen <me.clown.lemon@gmail.com>
2020-11-27 21:33:21 +0800

    :sparkles:  feat(Link): support new flavor, the 'react-router-dom' link

---
+++ packages/link/__test__/route-link.spec.tsx
@@ -0,0 +1,126 @@
+import React from 'react'
+import { BrowserRouter as Router } from 'react-router-dom'
+import { mount, render } from 'enzyme'
+import { DefaultTheme, ThemeProvider } from 'styled-components'
+import { RouteLink as Link } from '../src'
+
+
+describe('basic rendering case', () => {
+  const errorLogger = jest
+    .spyOn(global.console, 'error')
+    .mockImplementation((...args) => {
+      throw new Error(args.join(' '))
+    })
+
+  afterAll(() => {
+    errorLogger.mockRestore()
+  })
+
+  it('render a simple content', () => {
+    const text = 'Hello, world!'
+    const wrapper = render(
+      <Router>
+        <Link url="#">{ text }</Link>
+      </Router>
+    )
+    expect(wrapper.text()).toEqual(text)
+  })
+
+  it('render with custom className', () => {
+    const text = 'Hello, world!'
+    const className = 'custom-link'
+    const wrapper = render(
+      <Router>
+        <Link url="#" className={ className }>
+          <span>{ text }</span>
+        </Link>
+      </Router>
+    )
+    expect(wrapper.hasClass(className)).toEqual(true)
+    expect(wrapper.text()).toEqual(text)
+  })
+
+  it('url and children both are required', () => {
+    for (const value of [undefined, null] as any[]) {
+      expect(() => {
+        render(
+          <Router>
+            <Link url={ value }>link text</Link>
+          </Router>
+        )
+      }).toThrow(/Failed prop type: The prop `url` is marked as required/i)
+
+      expect(() => {
+        render(
+          <Router>
+            <Link url="/home">{ value }</Link>
+          </Router>
+        )
+      }).toThrow(/Failed prop type: The prop `children` is marked as required/i)
+    }
+  })
+
+  it('forward ref', () => {
+    const ref = React.createRef<HTMLAnchorElement>()
+    const wrapper = mount(
+      <Router>
+        <Link
+          ref={ ref }
+          url="/home"
+          data-value="waw"
+        >
+          1
+      </Link>
+      </Router>
+    )
+
+    const o = wrapper.getDOMNode()
+    expect(o).toEqual(ref.current)
+    expect(o.getAttribute('data-value')).toEqual('waw')
+  })
+
+  it('snapshot', () => {
+    const wrapper = render(
+      <Router>
+        <Link
+          url="/home"
+          title="home"
+          style={ { color: 'orange', fontSize: '16px' } }
+        >
+          some text1
+        <span>some text2</span>
+        </Link>
+      </Router>
+    )
+    expect(wrapper).toMatchSnapshot()
+  })
+
+  it('snapshot with theme', () => {
+    const theme: DefaultTheme = {
+      yozora: {
+        link: {
+          color: 'blue',
+          fontSize: undefined,
+          fontStyle: 'italic',
+          textDecoration: 'none',
+        }
+      }
+    }
+
+    const wrapper = render(
+      <Router>
+        <ThemeProvider theme={ theme }>
+          <Link
+            url="/home"
+            title="home"
+            style={ { color: 'orange', fontSize: '16px' } }
+          >
+            some text1
+          <span>some text2</span>
+          </Link>
+        </ThemeProvider>
+      </Router>
+    )
+    expect(wrapper).toMatchSnapshot()
+  })
+})
