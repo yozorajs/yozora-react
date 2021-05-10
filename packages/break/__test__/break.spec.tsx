dd3643aaea70ed278e33f1385d4c9bd06dcd3e17
guanghechen <me.guanghechen@gmail.com>
2021-05-10 17:34:00 +0800

    :sparkles:  feat: implement new component @yozora/react-break

---
+++ packages/break/__test__/break.spec.tsx
@@ -0,0 +1,43 @@
+import { render } from 'enzyme'
+import React from 'react'
+import Break from '../src'
+
+describe('prop types', function () {
+  beforeEach(() => {
+    jest.spyOn(global.console, 'error').mockImplementation((...args) => {
+      throw new Error(args.join(' '))
+    })
+  })
+
+  describe('className is optional', function () {
+    it('default', function () {
+      const node = render(<Break />)
+      expect(node.hasClass('yozora-break')).toEqual(true)
+    })
+
+    it('custom', function () {
+      const node = render(<Break className="my-break" />)
+      expect(node.hasClass('yozora-break')).toEqual(true)
+      expect(node.hasClass('my-break')).toEqual(true)
+    })
+  })
+
+  it('style is optional', function () {
+    const node = render(<Break style={{ color: 'orange' }} />)
+    expect(node.css('color')).toEqual('orange')
+  })
+})
+
+describe('snapshot', function () {
+  it('default', function () {
+    const wrapper = render(<Break />)
+    expect(wrapper).toMatchSnapshot()
+  })
+
+  it('custom', function () {
+    const wrapper = render(
+      <Break className="custom-class" style={{ color: 'orange' }} />,
+    )
+    expect(wrapper).toMatchSnapshot()
+  })
+})
