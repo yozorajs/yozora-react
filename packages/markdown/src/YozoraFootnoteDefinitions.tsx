e235cfe07709e26507fb1f19ac20bd2f42287bfc
guanghechen <me.guanghechen@gmail.com>
2022-01-15 18:47:22 +0800

    :art:  improve: use @yozora/react-footnote-definition instead of @yozora/react-footnote-definitions

--- packages/markdown/src/YozoraFootnoteDefinitions.tsx
+++ packages/markdown/src/YozoraFootnoteDefinitions.tsx
@@ -1,5 +1,5 @@
-import type { IFootnoteItem } from '@yozora/react-footnote-definitions'
-import YozoraFootnotesRenderer from '@yozora/react-footnote-definitions'
+import FootnoteDefinition from '@yozora/react-footnote-definition'
+import cn from 'clsx'
 import React from 'react'
 import { YozoraMarkdownContextType } from './context/context'
 import { YozoraNodesRenderer } from './YozoraNodesRenderer'
@@ -13,24 +13,41 @@ export interface IFootnoteDefinitionsProps {
    * If true, then the footnote definitions wont be render.
    */
   dontNeedFootnoteDefinitions?: boolean
+  /**
+   * Root css class of the component.
+   */
+  className?: string
+  /**
+   * Root css style.
+   */
+  style?: React.CSSProperties
 }
 
 export const YozoraFootnoteDefinitions: React.FC<IFootnoteDefinitionsProps> = props => {
-  const { footnoteDefinitionsTitle, dontNeedFootnoteDefinitions = false } = props
+  const { footnoteDefinitionsTitle, dontNeedFootnoteDefinitions = false, className, style } = props
 
   const { footnoteDefinitions } = React.useContext(YozoraMarkdownContextType)
 
-  const items: IFootnoteItem[] = React.useMemo(
+  const children = React.useMemo<React.ReactNode>(
     () =>
-      footnoteDefinitions.map((item, idx) => ({
-        label: item.label,
-        identifier: item.identifier,
-        children: <YozoraNodesRenderer key={idx} nodes={item.children} />,
-      })),
+      footnoteDefinitions.map((item, idx) => (
+        <FootnoteDefinition
+          key={idx}
+          label={item.label ?? item.identifier}
+          identifier={item.identifier}
+        >
+          <YozoraNodesRenderer nodes={item.children} />
+        </FootnoteDefinition>
+      )),
     [footnoteDefinitions],
   )
 
   if (dontNeedFootnoteDefinitions || footnoteDefinitions.length <= 0) return null
 
-  return <YozoraFootnotesRenderer nodes={items} title={footnoteDefinitionsTitle} />
+  return (
+    <div className={cn('yozora-footnote-definitions', className)} style={style}>
+      <div className="yozora-footnote-definitions__title">{footnoteDefinitionsTitle}</div>
+      <ul className="yozora-footnote-definitions__main">{children}</ul>
+    </div>
+  )
 }
