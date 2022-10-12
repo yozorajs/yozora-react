3eb8aa39354094e72217fb13f6e75b85c714c567
guanghechen <me.guanghechen@gmail.com>
2022-10-12 11:05:55 +0800

    :white_check_mark:  test: update jest configrautions & update test snapshots

--- jest.config.js
+++ jest.config.js
@@ -1,39 +1,17 @@
+const { tsMonorepoConfig } = require('@guanghechen/jest-config')
 const path = require('path')
-const { compilerOptions } = require('./tsconfig')
 
-const moduleNameMapper = {}
-for (const moduleName of Object.getOwnPropertyNames(compilerOptions.paths)) {
-  const paths = compilerOptions.paths[moduleName].map(p => path.resolve(__dirname, p))
-  let pattern = '^' + moduleName.replace(/[-\\^$*+?.()|[\]{}]/g, '\\$&') + '$'
-  moduleNameMapper[pattern] = paths.length === 1 ? paths[0] : paths
-}
+const baseConfig = tsMonorepoConfig(__dirname)
 
 module.exports = {
-  bail: true,
-  verbose: true,
+  ...baseConfig,
   testEnvironment: 'jsdom',
-  errorOnDeprecated: true,
-  roots: ['<rootDir>/src', '<rootDir>/__test__'],
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   moduleNameMapper: {
-    ...moduleNameMapper,
+    ...baseConfig.moduleNameMapper,
     '\\.styl$': path.join(__dirname, 'jest.identity-obj-proxy.js'),
-  },
-  globals: {
-    'ts-jest': {
-      tsconfig: '<rootDir>/tsconfig.json',
-    },
   },
   setupFilesAfterEnv: ['<rootDir>/../../jest.setupEnzyme.ts'],
   snapshotSerializers: ['enzyme-to-json/serializer'],
-  transform: {
-    '^.+\\.tsx?$': 'ts-jest',
-  },
-  testURL: 'http://localhost/',
-  testRegex: '/(__test__)/[^/]+\\.spec\\.tsx?$',
-  testPathIgnorePatterns: ['/coverage/', '/lib/', '/node_modules/'],
-  collectCoverage: false,
-  coverageDirectory: '<rootDir>/coverage/',
   collectCoverageFrom: [
     '<rootDir>/index.js',
     '<rootDir>/src/**/*.{js,jsx,ts,tsx}',
@@ -48,5 +26,4 @@ module.exports = {
       statements: 90,
     },
   },
-  coverageReporters: ['lcov', 'text', 'text-summary'],
 }
